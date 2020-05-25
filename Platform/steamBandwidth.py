d85343567079b98ab954904e70be9fa2d3d59282
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-25 21:29:27 +0300

    AVG calc in progress

--- Platform/steamBandwidth.py
+++ Platform/steamBandwidth.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import math
 
 class SteamBandwidth():
     def __init__(self, weird_num):
@@ -9,20 +10,33 @@ class SteamBandwidth():
         self.response = requests.get(self.url).text
         self.bandwidthFILENAME = './DATA(json)/bandwidthSteamData.json'
 
+    def convert_size(self, size_bytes):
+        if size_bytes == 0:
+            return "0B"
+        for count in ['Bytes', 'KB', 'MB', 'GB']:
+            if size_bytes > -1024.0 and size_bytes < 1024.0:
+                print ("%3.1f%s" % (size_bytes, count))
+            size_bytes /= 1024.0
+        print("%3.1f%s" % (size_bytes, 'TB'))
+
     def setup(self):
         startidx = self.response.find('(')
         endidx = self.response.find(')')
         bandwidthFile = json.loads(self.response[startidx + 1:endidx])
-        print(bandwidthFile['USA']['totalbytes'])
 
         for name in bandwidthFile:
+            total = 0
             if type(bandwidthFile[name]['totalbytes']) == str:
                 bandwidthFile[name]['totalbytes'] = int(bandwidthFile[name]['totalbytes'])
+                scale = bandwidthFile[name]['totalbytes']
+                # grand_total += bandwidthFile[name]['totalbytes']
             if type(bandwidthFile[name]['avgmbps']) == str:
                 bandwidthFile[name]['avgmbps'] = int(bandwidthFile[name]['avgmbps'])
         return bandwidthFile
 
     def writeBandwidthSteam(self):
-        bandwidthFile = self.setup()
-        with open(self.bandwidthFILENAME, 'w') as outfile:
-            json.dump(bandwidthFile, outfile)
+        grand_total = 12000
+        self.convert_size(grand_total)
+        # bandwidthFile = self.setup()
+        # with open(self.bandwidthFILENAME, 'w') as outfile:
+        #     json.dump(bandwidthFile, outfile)
--- control.py
+++ control.py
@@ -13,6 +13,7 @@ from Scrapers.finance import *
 # steam_general = SteamList('2C2C2E0FEBFD8D32F9346602D47C83BA')
 # steam_general.writeToJSON()
 
+# Write AVG Download rate, 
 steam_bandwidth = SteamBandwidth(17) # 16 = is the weird number at the end of the request link
 steam_bandwidth.writeBandwidthSteam()
 
