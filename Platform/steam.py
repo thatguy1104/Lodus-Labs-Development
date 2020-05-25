9bc575f9ccf5a282f55dfc9b4d61d29ecd5351c1
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-25 15:31:21 +0300

    structure updated

--- Platform/steam.py
+++ Platform/steam.py
@@ -1,3 +1,15 @@
-class SteamList():
-    self.file = 'Scrapers/Game_Corp/corp_info.json'
+import json
+import os
 
+class SteamList():
+    def __init__(self):
+        self.filename = "corp_info.json"
+
+    def readCorpInfo(self):
+        with open(self.filename) as json_file:
+            data = json.load(json_file)
+            for p in data['company']:
+                print(p)
+
+steam = SteamList()
+steam.readCorpInfo()
