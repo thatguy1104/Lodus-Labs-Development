6c5a49ca1e9af7acda9f955237b530ccd09af561
Dan <danalk61@gmail.com>
2020-05-27 21:42:37 +0200

    authetnication added

--- TWITCH/twitch_integration.py
+++ TWITCH/twitch_integration.py
@@ -3,10 +3,12 @@ import requests, json, sys
 BASE_URL = 'https://api.twitch.tv/helix/'
 CLIENT_ID = 'lgb4dq2tni1ce2ib9gzmg0kp571lgz'
 CLIENT_SECRET = '3j58umfzeu2mve615u3mkfwogfamy1'
+GRANT_TYPE = 'client_credentials'
 HEADERS = { 'Client-ID': CLIENT_ID, 
             'Client-Secret': CLIENT_SECRET,
-            'Token-Type': "bearer"}
+            'Authorization': 'Bearer t5gsvxikp6cnmfqmde9e0lncus8ejs'}
 INDENT = 2
+
 
 def get_response(query):
     """
@@ -45,7 +47,25 @@ def get_user_videos_query(user_id):
 def get_games_query():
   return 'games/top'
 
-user_login = 'ninja'
-query = get_user_streams_query(user_login)
-response = get_response(query)
-print_response(response)
+def get_oath2_verification():
+    test = 0
+    return test
+
+#user_login = 'ninja'
+#query = get_user_streams_query(user_login)
+#response = get_response(query)
+#print(response.url)
+#print_response(response)
+
+x = requests.get('https://api.twitch.tv/helix/games/top', headers=HEADERS)
+#print_response(x)
+y = x.json()
+
+data = y
+with open('test.json', 'w') as f:
+    json.dump(y, f, indent=INDENT)
+print(y["data"][0]["name"])
+
+#r = requests.post('https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type={}'.format(CLIENT_ID, CLIENT_SECRET, GRANT_TYPE))
+#r_json = r.json()
+#print(json.dumps(r_json, indent=INDENT))
