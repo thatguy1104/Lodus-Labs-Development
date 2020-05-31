102ea0d23fe0d762dab78b9d05ecf650b4089029
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-31 12:47:06 +0300

    dev info in progress

--- Developers&Games/gameStats.py
+++ Developers&Games/gameStats.py
@@ -3,6 +3,64 @@ import json
 from bs4 import BeautifulSoup
 import lxml
 
-url = 'https://www.androidrank.org/api/application/com.whatsapp?key=9GmLbwMLMgxz3BeAo3Two0NzeuRVN8Jin6EPcerWVfesQH4BhLuLeUSlETXfrnMd'
-response = requests.get(url)
-print(response)
+class AllGamesForDev():
+    def __init__(self):
+        self.link = 'https://www.androidrank.org'
+        self.writeFILE = 'allStats.json'
+        self.readFILE = 'devRanks.json'
+
+    def scrapeOne(self, id):
+        response = requests.get(self.link + id)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        odd = soup.find_all('tr', class_='odd')
+        even = soup.find_all('tr', class_='even')
+        all_rows = odd + even
+
+        results = []
+        for item in all_rows:
+            row = item.find_all('td')
+            rank = int(row[0].text.replace('.', ''))
+            tittle = row[1].text.replace('\n', '')
+            rating = row[3].text
+            intalls = row[4].text
+            avg_rating = float(row[5].text)
+            growth_30_days = row[6].text
+            growth_60_days = row[7].text
+            price = row[8].text.replace('\n', '')
+            results.append((rank, tittle, rating, intalls, avg_rating, growth_30_days, growth_60_days, price))
+
+        return results
+
+    def getIDs(self):
+        ids = []
+        with open(self.readFILE) as json_file:
+            data = json.load(json_file)
+            for p in data:
+                print('Name: ' + p)
+
+
+    def getAllGameStats(self):
+        id = ['/developer?id=5700313618786177705']
+        data = {}
+
+
+        resultOne = self.scrapeOne(id[0])
+        for i in range(len(resultOne)):
+            data[resultOne[i][1]] = []
+            data[resultOne[i][1]].append({
+                'Rank'              : resultOne[i][0],
+                'App Name'          : resultOne[i][1],
+                'Total Rating'      : resultOne[i][2],
+                'Installs'          : resultOne[i][3],
+                'Average Rating'    : resultOne[i][4],
+                'Growth: 30 days'   : resultOne[i][5],
+                'Growth: 60 days'   : resultOne[i][6],
+                'Price'             : resultOne[i][7]
+            })
+
+        with open(self.writeFILE, 'w') as outfile:
+            json.dump(data, outfile)
+
+obj = AllGamesForDev()
+obj.getAllGameStats()
