470cb23681bf9d7625932532454d97b47049b992
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-06-09 00:57:28 +0300

    postgre db connect

--- DATABASE/insert.py
+++ DATABASE/insert.py
@@ -1,26 +1,36 @@
 import psycopg2
 
-conn = psycopg2.connect(database="d48rv6m38ss0k6", user="nllltklgitidxv",
-                        password="95aa107e5287dda564e9021e01c05298d26a2f2b455629b489ee2cd36987ed89", host="ec2-54-247-78-30.eu-west-1.compute.amazonaws.com", port="5432")
-print ("Opened database successfully")
 
-cur = conn.cursor()
-
-# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-#       VALUES (1, 'Paul', 32, 'California', 20000.00 )")
-
-# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-#       VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
-
-# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-#       VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
-
-# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-#       VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
     
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (5, 'Albert', 19, 'Moscow ', 100000.00 )")
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect("dbname=template1 user=postgres password=hakalbert0")
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
 
-conn.commit()
-print ("Records created successfully")
-conn.close()
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	    # close the communication with the PostgreSQL
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+if __name__ == '__main__':
+    connect()
