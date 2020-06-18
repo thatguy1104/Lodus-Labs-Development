4e2501839a7f96338a3b7b3e1a1ee029630a7618
Dan <danalk61@gmail.com>
2020-06-18 21:17:20 +0200

    twitch changed

--- TWITCH/twitch_oauth2_access_token_generator.py
+++ TWITCH/twitch_oauth2_access_token_generator.py
@@ -1,7 +1,9 @@
 import requests, json, sys
 
-# Returns oauth2 access token
 def get_access_token(client_id, client_secret, grant_type):
+    """
+    Returns oauth2 access token
+    """
     r = requests.post('https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type={}'
     .format(client_id, client_secret, grant_type))
     r_json = r.json() # For debugging use: print(json.dumps(r_json, indent=INDENT))
