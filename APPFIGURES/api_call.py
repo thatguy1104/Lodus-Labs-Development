6a3fb609b0fbb372dc415ca517f46483c05750fb
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-07-24 15:49:28 +0300

    load appfigures

--- APPFIGURES/api_call.py
+++ APPFIGURES/api_call.py
@@ -6,9 +6,12 @@ secret_key = '28e000e4ec6d43018c674f7822346b3b'
 
 url = 'https://api.appfigures.com/v2/data/countries?client_key=5f535edf1194429095216a6d05b3343b'
 
-response = requests.get(url).json()
+response = requests.get(url, auth=('zcabukh@ucl.ac.uk', 'hakalbert0')).json()
 
 with open('results.json', 'w') as outfile:
     json.dump(response, outfile, indent=5)
 
 
+# class AppFigures:
+#     def __init__(self):
+#         super().__init__()
